ede3a37ff5406f59fdc9bbff8206c2d264053d43
Vu Luong <vul@Liberty-MacBook-Pro-M1.local>
2025-01-11 18:14:43 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hi i'm Trey
+          Hi i'm Trey, I'm a software engineer. I'm learning how to deploy a react app to AWS S3.
         </a>
       </header>
     </div>
