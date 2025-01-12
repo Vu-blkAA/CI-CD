10810a1346fb6e57481447a883070175c64aaede
Vu-blkaa <vublkaa@gmail.com>
2025-01-12 10:26:51 +0700

    udppate

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hi i'm Trey, I'm a software engineer. I'm learning how to deploy a react app to AWS S3 hihihaha
+          Hi i'm Trey, I'm a software engineer. I'm learning how to deploy a react app to AWS S3
         </a>
       </header>
     </div>
