26426b78ac307f52f9d3a57fd8d054885f2a619c
Vu Luong <vul@Liberty-MacBook-Pro-M1.local>
2025-01-14 11:57:44 +0700

    update yml

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 const TREY_LINK = import.meta.env.VITE_HRM_GOOGLE;
+const TECH_DOMAIN = import.meta.env.TECH_DOMAIN;
 
 function App() {
   console.log('environment', TREY_LINK);
+  console.log('tech domain', TECH_DOMAIN);
+
   return (
     <div className="App">
       <header className="App-header">
